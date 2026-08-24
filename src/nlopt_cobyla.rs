c26dfc99519fee3bf965b519031207c426a20f88
relf <remi.lafage@onera.fr>
2026-08-24 16:52:59 +0200

    Linting

--- src/nlopt_cobyla.rs
+++ src/nlopt_cobyla.rs
@@ -40,8 +40,7 @@ pub(crate) fn nlopt_function_raw_callback<F: Func<T>, T>(
 
     // recover FunctionCfg object from supplied params and call
     let f = unsafe { &mut *(params as *mut NLoptFunctionCfg<F, T>) };
-    let res = (f.objective_fn)(argument, &mut f.user_data);
-    res
+    (f.objective_fn)(argument, &mut f.user_data)
 }
 
 pub(crate) fn nlopt_constraint_raw_callback<F: Func<T>, T>(
